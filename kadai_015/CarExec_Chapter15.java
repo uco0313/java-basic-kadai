e10808e171b7382b388165e847dd58f97b6b5cb3
uco0313 <uco0313@gmail.com>
2025-09-04 17:28:10 +0900

    車クラスを作って速度を出力しようを提出

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,11 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+    public static void main(String[] args) {
+     
+        Car_Chapter15 car = new Car_Chapter15(1, 10);
+        
+        car.changeGear(3);
+        car.run();
+    }
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,31 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	private int gear = 1; 
+	private int speed = 10; 
+
+
+	public Car_Chapter15(int gear, int speed) {
+		this.gear = gear;
+		this.speed = speed;
+	}
+
+	public void changeGear(int afterGear) {
+		int beforeGear = this.gear; 
+		this.gear = afterGear;
+		switch (afterGear) {
+			case 1 -> this.speed = 10;
+			case 2 -> this.speed = 20;
+			case 3 -> this.speed = 30;
+			case 4 -> this.speed = 40;
+			case 5 -> this.speed = 50;
+			default -> this.speed = 10;
+		}
+
+		System.out.println("ギア" + beforeGear + "から" + afterGear + "に切り替えました");
+	}
+
+	public void run() {
+		System.out.println("速度は時速" + this.speed + "kmです");
+	}
+}
